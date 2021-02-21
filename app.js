d8e10d7b97868899412058c95066138f04249c19
rashedulalam362 <rashedulalam362@gmail.comgit config --global user.name rashedulalam362git commit -mfirst git config --global user.email rashedulalam362@gmail.com>
2021-02-21 23:23:32 +0600

    finished

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('doration').value || 1000;
+  const duration = document.getElementById('duration').value || 1000;
   if (duration<0){
     alert('negative values not allowed')
     return;
