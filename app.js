48b0506de663bdcd96f99f588308b11660326260
rashedulalam362 <rashedulalam362@gmail.comgit config --global user.name rashedulalam362git commit -mfirst git config --global user.email rashedulalam362@gmail.com>
2021-02-21 21:39:56 +0600

    update

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner(false)
   })
 
 }
@@ -37,6 +38,7 @@ const getImages = (query) => {
     .then(response => response.json())
     .then(data => showImages(data.hits))
     .catch(err => console.log(err))
+  
  
 }
 
@@ -126,6 +128,7 @@ searchBtn.addEventListener('click', function () {
   const search = document.getElementById('search');
   getImages(search.value)
   sliders.length = 0;
+
 })
 
 
